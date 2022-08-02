5bedd33d8c77300644d6fab22aab935da35dae83
PawelDat <pawel.datko2000@gmail.com>
2022-08-03 01:03:23 +0200

    coin geko implementation

--- src/App.js
+++ src/App.js
@@ -1,11 +1,25 @@
-
-import './App.css';
+import React,{useState,useEffect} from "react";
+import axios from "axios";
+import Coins from "./components/Coins";
 
 function App() {
+
+  const [coins,setCoins] = useState ([])
+
+  const url = 'https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=10&page=1&sparkline=false'
+
+  useEffect(() => {
+    axios.get(url).then((response) => {
+      setCoins(response.data) 
+      console.log(response.data[0])
+    }).catch((error) => {
+      console.log(error)
+    })
+  }, [])
+
   return (
     <div className="App">
-      <header className="App-header">
-      </header>
+    <Coins coins={coins}/>
     </div>
   );
 }
